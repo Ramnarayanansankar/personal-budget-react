89250da5840b45252bf79983e317d9ca2bccb056
Ramnarayamsankar <ramnarayamsankar@gmail.com>
2024-09-29 13:07:39 -0400

    Brought Personal Budget code in React

---
+++ src/Menu/Menu.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+import{
+    Link
+  
+  } from "react-router-dom";
+
+function Menu() {
+  return ( 
+    <nav
+        role="navigation"
+        aria-label="Main menu"
+        itemscope
+        itemType="https://schema.org/SiteNavigationElement"
+
+
+    >  
+         <ul>
+                <li><Link itemProp="url" to="/" title=" Home Budget">Home</Link></li>
+                <li><Link itemProp="url" to="/about">About</Link></li>
+                <li><Link itemProp="url" to="/login">Login</Link></li>
+                <li><Link itemProp="url" to="https://google.com" title=" Search google for interesting things">Google</Link></li>
+       
+        </ul>
+    </nav>
+  );
+}
+
+export default Menu;
